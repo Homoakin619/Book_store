8d0219ea234bee2f924f1fcd2bad367e3bf40212
Homoakin619 <homohaykins619@gmail.com>
2022-03-24 02:35:08 +0100

    add changes

--- ebook/settings.py
+++ ebook/settings.py
@@ -17,12 +17,15 @@ import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env()
+environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-535f!(h$%57ic@tb3l+s4z$uq^zgz(m%xss&44lt017)juvul^'
+
+SECRET_KEY = env(‘SECRET_KEY’)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
